fc6cd422c4d46134285d084f4bec908c7346d8fe
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-06-27 12:53:18 +1000

    Crowdrender2d

--- src/visualfx/crowdrender2d.cpp
+++ src/visualfx/crowdrender2d.cpp
@@ -1,17 +1,17 @@
 #include "common.h"
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdRender2D_cCrowdRender2D);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_cCrowdRender2D);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdRender2D__cCrowdRender2D);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D__cCrowdRender2D);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdRender2D_init);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_init);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdRender2D_purge);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_purge);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdRender2D_constructCrowdAnim2D);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_constructCrowdAnim2D);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdAnim2D_cCrowdAnim2D);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_cCrowdAnim2D);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdAnim2D_advanceFrame);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_advanceFrame);
 
-INCLUDE_ASM("/mnt/g/ssx3/asm/nonmatchings/visualfx/crowdrender2d", cCrowdAnim2D_update);
+INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_update);
