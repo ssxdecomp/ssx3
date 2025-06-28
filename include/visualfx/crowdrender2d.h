196e560d6687912465b6713677ff5234d17cd009
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-06-28 11:29:43 +1000

    Crwodrender2D Update

--- include/visualfx/crowdrender2d.h
+++ include/visualfx/crowdrender2d.h
@@ -2,6 +2,10 @@
 #define CROWDRENDER2D_H
 
 #include "common.h"
-
+namespace cCrowdRender2D
+{
+	void constructCrowdAnim2D(int param_1);
+	void cCrowdAnim2D(void* pvVar1, int param_1);
+}
 
 #endif /* CROWDRENDER2D_H */
--- src/visualfx/crowdrender2d.cpp
+++ src/visualfx/crowdrender2d.cpp
@@ -1,17 +1,40 @@
-#include "common.h"
+#include "visualfx/crowdrender2d.h"
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_cCrowdRender2D);
+namespace cCrowdRender2D
+{
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_cCrowdRender2D);
+	int cCrowdRender2D_cCrowdRender2D(int param_1) {
+		cCrowdRender2D_init();
+		return param_1;
+	}
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D__cCrowdRender2D);
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D__cCrowdRender2D);
+	void constructCrowdAnim2D(int param_1)
+	{
+		void* pvVar1;
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_init);
+		//pvVar1 = fun_00317d70(0x20, 0x4875d8, 0x20000000, 0);
+		//cCrowdAnim2D(pvVar1, param_1);
+		return;
+	}
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_purge);
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_init);
+	void cCrowdRender2D_init()
+	{
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_constructCrowdAnim2D);
+	}
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_cCrowdAnim2D);
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_purge);
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_advanceFrame);
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_constructCrowdAnim2D);
 
-INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_update);
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_cCrowdAnim2D);
+	void cCrowdAnim2D(void* pvVar1,int param_1)
+	{
+
+	}
+
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_advanceFrame);
+
+	INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_update);
+}
