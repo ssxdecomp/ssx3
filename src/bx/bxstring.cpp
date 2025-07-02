3026fb62713c2ecfb0bd8212df16edbb3b89f650
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-07-02 19:38:44 +1000

    bxString Symbols

--- src/bx/bxstring.cpp
+++ src/bx/bxstring.cpp
@@ -1,89 +1,89 @@
 #include "common.h"
 
-INCLUDE_ASM("bxstring", func_00317FE8);
+INCLUDE_ASM("bx/bxstring", cBXString_cBXString1);
 
-INCLUDE_ASM("bxstring", func_00318040);
+INCLUDE_ASM("bx/bxstring", cBXString_Realloc);
 
-INCLUDE_ASM("bxstring", func_003180B8);
+INCLUDE_ASM("bx/bxstring", cBXString_Reset);
 
-INCLUDE_ASM("bxstring", func_00318120);
+INCLUDE_ASM("bx/bxstring", func_00318120);
 
-INCLUDE_ASM("bxstring", func_00318188);
+INCLUDE_ASM("bx/bxstring", cBXString_Resize);
 
-INCLUDE_ASM("bxstring", func_003181E8);
+INCLUDE_ASM("bx/bxstring", func_003181E8);
 
-INCLUDE_ASM("bxstring", func_00318260);
+INCLUDE_ASM("bx/bxstring", cBXString__cBXString);
 
-INCLUDE_ASM("bxstring", func_003182D8);
+INCLUDE_ASM("bx/bxstring", cBXString_cBXString2);
 
-INCLUDE_ASM("bxstring", func_00318350);
+INCLUDE_ASM("bx/bxstring", func_00318350);
 
-INCLUDE_ASM("bxstring", func_003183D8);
+INCLUDE_ASM("bx/bxstring", cBXString_InitFromCString);
 
-INCLUDE_ASM("bxstring", func_00318448);
+INCLUDE_ASM("bx/bxstring", cBXString_operatorE);
 
-INCLUDE_ASM("bxstring", func_003184E8);
+INCLUDE_ASM("bx/bxstring", cBXString_cBXString4);
 
-INCLUDE_ASM("bxstring", func_00318540);
+INCLUDE_ASM("bx/bxstring", func_00318540);
 
-INCLUDE_ASM("bxstring", func_003185C8);
+INCLUDE_ASM("bx/bxstring", func_003185C8);
 
-INCLUDE_ASM("bxstring", func_00318630);
+INCLUDE_ASM("bx/bxstring", func_00318630);
 
-INCLUDE_ASM("bxstring", func_003186D0);
+INCLUDE_ASM("bx/bxstring", func_003186D0);
 
-INCLUDE_ASM("bxstring", func_00318760);
+INCLUDE_ASM("bx/bxstring", func_00318760);
 
-INCLUDE_ASM("bxstring", func_003187C8);
+INCLUDE_ASM("bx/bxstring", func_003187C8);
 
-INCLUDE_ASM("bxstring", func_00318830);
+INCLUDE_ASM("bx/bxstring", cBXString_ConcatImpl);
 
-INCLUDE_ASM("bxstring", func_00318910);
+INCLUDE_ASM("bx/bxstring", cBXString_Concat);
 
-INCLUDE_ASM("bxstring", func_00318968);
+INCLUDE_ASM("bx/bxstring", func_00318968);
 
-INCLUDE_ASM("bxstring", func_003189A0);
+INCLUDE_ASM("bx/bxstring", func_003189A0);
 
-INCLUDE_ASM("bxstring", func_003189D0);
+INCLUDE_ASM("bx/bxstring", func_003189D0);
 
-INCLUDE_ASM("bxstring", func_00318A88);
+INCLUDE_ASM("bx/bxstring", func_00318A88);
 
-INCLUDE_ASM("bxstring", func_00318AF8);
+INCLUDE_ASM("bx/bxstring", func_00318AF8);
 
-INCLUDE_ASM("bxstring", func_00318B40);
+INCLUDE_ASM("bx/bxstring", func_00318B40);
 
-INCLUDE_ASM("bxstring", func_00318BC0);
+INCLUDE_ASM("bx/bxstring", func_00318BC0);
 
-INCLUDE_ASM("bxstring", func_00318BF8);
+INCLUDE_ASM("bx/bxstring", func_00318BF8);
 
-INCLUDE_ASM("bxstring", func_00318C18);
+INCLUDE_ASM("bx/bxstring", cBXString_FindLastOf);
 
-INCLUDE_ASM("bxstring", func_00318C60);
+INCLUDE_ASM("bx/bxstring", func_00318C60);
 
-INCLUDE_ASM("bxstring", func_00318CA0);
+INCLUDE_ASM("bx/bxstring", cBXString_FindFirstOf);
 
-INCLUDE_ASM("bxstring", func_00318CE8);
+INCLUDE_ASM("bx/bxstring", func_00318CE8);
 
-INCLUDE_ASM("bxstring", func_00318D28);
+INCLUDE_ASM("bx/bxstring", func_00318D28);
 
-INCLUDE_ASM("bxstring", func_00318D90);
+INCLUDE_ASM("bx/bxstring", func_00318D90);
 
-INCLUDE_ASM("bxstring", func_00318DF8);
+INCLUDE_ASM("bx/bxstring", func_00318DF8);
 
-INCLUDE_ASM("bxstring", func_00318E68);
+INCLUDE_ASM("bx/bxstring", func_00318E68);
 
-INCLUDE_ASM("bxstring", func_00318EB8);
+INCLUDE_ASM("bx/bxstring", func_00318EB8);
 
-INCLUDE_ASM("bxstring", func_00318F20);
+INCLUDE_ASM("bx/bxstring", func_00318F20);
 
-INCLUDE_ASM("bxstring", func_00318F58);
+INCLUDE_ASM("bx/bxstring", func_00318F58);
 
-INCLUDE_ASM("bxstring", func_00318F90);
+INCLUDE_ASM("bx/bxstring", func_00318F90);
 
-INCLUDE_ASM("bxstring", func_00319028);
+INCLUDE_ASM("bx/bxstring", func_00319028);
 
-INCLUDE_ASM("bxstring", func_003190A8);
+INCLUDE_ASM("bx/bxstring", cBXString_cBXString5);
 
-INCLUDE_ASM("bxstring", func_00319120);
+INCLUDE_ASM("bx/bxstring", func_00319120);
 
-INCLUDE_ASM("bxstring", func_00319170);
+INCLUDE_ASM("bx/bxstring", func_00319170);
