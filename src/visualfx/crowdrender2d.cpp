fae20f21af5b1564baee9a25605e104228d32e5d
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-07-08 18:30:58 +1000

    Update crowdrender2d.cpp

--- src/visualfx/crowdrender2d.cpp
+++ src/visualfx/crowdrender2d.cpp
@@ -44,7 +44,19 @@ int cCrowdRender2D_purge(int *param_1)
 }
 #endif
 
+//77.65%
+//https://decomp.me/scratch/PwEN9
 INCLUDE_ASM("visualfx/crowdrender2d", cCrowdRender2D_constructCrowdAnim2D);
+//#ifdef SKIP_ASM
+//void cCrowdRender2D_constructCrowdAnim2D(long param_1)
+//{
+//	void* pvVar1;
+//
+//	pvVar1 = cMemMan_alloc(0x20, s_cCrowdAnim2D, 0x20000000, 0);
+//	cCrowdAnim2D_cCrowdAnim2D(pvVar1, param_1);
+//	return;
+//}
+//#endif
 
 INCLUDE_ASM("visualfx/crowdrender2d", cCrowdAnim2D_cCrowdAnim2D);
 #ifdef SKIP_ASM
